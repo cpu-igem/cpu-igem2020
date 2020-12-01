08a3ef1af4e5c0063cc5f99d80277967d50c43d7
LiuXin <15736409668@163.com>
2020-12-01 16:44:25 +0800

    add

--- src/pages/content/Contribution.tsx
+++ src/pages/content/Contribution.tsx
@@ -13,7 +13,7 @@ const content = (<>
         <h1>Contribution</h1>
         <p>This year, we provided new data for an existing part. PalcA-Regulated GFP Expression Construct（BBa_K2539500）, which detailed information is described in the part registrations page. </p>
         <p>Our results matched the general expected trend (Figure 1). Addition of 1% (1000X) ethanol (EtOH) concentration seemed to yield the highest fluorescence, and lower amounts of 0.1%; and 0.2% ethanol were not significantly different than the controls. Over 0.4% ethanol seemed to kill the bacteria (5 mL of culture), which matches literature thresholds of ethanol tolerance [1]. In our tests, 1% ethanol yielded the greatest amount of GFP but this effect was quite weak . In summary,the ethanol promoter, PalcA, is functional in the presence of a specific concentration of ethanol, but is quite inefficient.</p>
-        <p><ImageWithShadow src={con.Figure1$png} noShadow caption='Figure 1.GFP Fluorescence Intensity of E. coli cultures with varying amounts of ethanol.' /></p>
+        <p><ImageWithShadow src='https://2020.igem.org/wiki/images/9/99/T--CPU_CHINA--2a3bf1ae.png' noShadow width='60%' caption='Figure 1.GFP Fluorescence Intensity of E. coli cultures with varying amounts of ethanol.' /></p>
 
         <p>Click the <Link href='http://parts.igem.org/Part:BBa_K2539500'>BBa_K2539500</Link> to see detailed information! </p>
 
